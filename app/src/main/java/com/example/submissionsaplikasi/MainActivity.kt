62ee204371af8caf2ab7f56119ca3f57738ea567
Noirsen <profzumar@gmail.com>
2025-10-15 09:00:30 +0700

    komen

--- app/src/main/java/com/example/submissionsaplikasi/MainActivity.kt
+++ app/src/main/java/com/example/submissionsaplikasi/MainActivity.kt
@@ -38,6 +38,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+//tambahan komen untuk percobaan sesuatu
 @Composable
 fun RegistrationScreen() {
     var firstName by rememberSaveable { mutableStateOf("") }
